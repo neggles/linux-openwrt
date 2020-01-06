bf5e6810fe2a4c6e491b576f02391f7ec3262ffb
Koen Vandeputte <koen.vandeputte@ncentric.com>
2020-01-06 13:07:56 +0100

    mtd: spi-nor: add support for Gigadevice GD25D05
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- drivers/mtd/spi-nor/gigadevice.c
+++ drivers/mtd/spi-nor/gigadevice.c
@@ -24,6 +24,9 @@ static struct spi_nor_fixups gd25q256_fixups = {
 };
 
 static const struct flash_info gigadevice_parts[] = {
+	{ "gd25q05", INFO(0xc84010, 0, 64 * 1024,  1,
+			  SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			  SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
 	{ "gd25q16", INFO(0xc84015, 0, 64 * 1024,  32,
 			  SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			  SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
