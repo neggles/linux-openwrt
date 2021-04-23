ef448b4a1260590e7fb63c3df80d47ded198a33c
Ilya Lipnitskiy <ilya.lipnitskiy@gmail.com>
2021-04-22 22:20:56 -0700

    net: ethernet: mtk_eth_soc: fix build_skb cleanup
    In case build_skb fails, call skb_free_frag on the correct pointer. Also
    update the DMA structures with the new mapping before exiting, because
    the mapping was successful
    
    Suggested-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Ilya Lipnitskiy <ilya.lipnitskiy@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -1325,9 +1325,9 @@ static int mtk_poll_rx(struct napi_struct *napi, int budget,
 		/* receive data */
 		skb = build_skb(data, ring->frag_size);
 		if (unlikely(!skb)) {
-			skb_free_frag(new_data);
+			skb_free_frag(data);
 			netdev->stats.rx_dropped++;
-			goto release_desc;
+			goto skip_rx;
 		}
 		skb_reserve(skb, NET_SKB_PAD + NET_IP_ALIGN);
 
@@ -1347,6 +1347,7 @@ static int mtk_poll_rx(struct napi_struct *napi, int budget,
 		skb_record_rx_queue(skb, 0);
 		napi_gro_receive(napi, skb);
 
+skip_rx:
 		ring->data[idx] = new_data;
 		rxd->rxd1 = (unsigned int)dma_addr;
 
